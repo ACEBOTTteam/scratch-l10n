07e162c9c11c386ae9b39e3f02781011d50fb487
ACEBOTTteam <info@acebott.com>
2025-04-10 16:28:47 +0800

    ✨ feat: 添加印度语

--- modify/alterAndAdd.js
+++ modify/alterAndAdd.js
@@ -21,95 +21,138 @@ const fs = require('fs')
  * 
  */
 
-const block = {}
+const block = {
+
+}
 
 const extensions = {
-    "zh-cn": {
-        "carMotor.bluetoothController.multithreading": "多线程"
-    },
-    "zh-tw": {
-        "carMotor.bluetoothController.multithreading": "多執行緒"
-    },
-    "en": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "fr": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "ja": {
-        "carMotor.bluetoothController.multithreading": "マルチスレッド"
-    },
-    "de": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "es": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "ru": {
-        "carMotor.bluetoothController.multithreading": "многопоточное выполнение"
-    },
-    "cs": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "it": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "pl": {
-        "carMotor.bluetoothController.multithreading": "wielowątkowość"
-    },
-    "tr": {
-        "carMotor.bluetoothController.multithreading": "çok iş parçacıklı"
-    },
-    "pt": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "hr": {
-        "carMotor.bluetoothController.multithreading": "višenitno"
-    },
-    "ko": {
-        "carMotor.bluetoothController.multithreading": "멀티스레딩"
-    },
-    "th": {
-        "carMotor.bluetoothController.multithreading": "มัลติเธรด"
-    },
-    "nl": {
-        "carMotor.bluetoothController.multithreading": "multithreading"
-    },
-    "ar": {
-        "carMotor.bluetoothController.multithreading": "تعدد مؤشرات الترابط"
-    }
+  "zh-cn": {
+    "ASR.LOW": "低",
+    "ASR.MIDDLE": "中",
+    "ASR.HIGH": "高",
+  },
+  "zh-tw": {
+    "ASR.LOW": "低",
+    "ASR.MIDDLE": "中",
+    "ASR.HIGH": "高"
+  },
+  "en": {
+    "ASR.LOW": "low",
+    "ASR.MIDDLE": "middle",
+    "ASR.HIGH": "high"
+  },
+  "fr": {
+    "ASR.LOW": "faible",
+    "ASR.MIDDLE": "moyen",
+    "ASR.HIGH": "élevé"
+  },
+  "ja": {
+    "ASR.LOW": "低い",
+    "ASR.MIDDLE": "中",
+    "ASR.HIGH": "高い"
+  },
+  "de": {
+    "ASR.LOW": "niedrig",
+    "ASR.MIDDLE": "mittel",
+    "ASR.HIGH": "hoch"
+  },
+  "es": {
+    "ASR.LOW": "bajo",
+    "ASR.MIDDLE": "medio",
+    "ASR.HIGH": "alto"
+  },
+  "ru": {
+    "ASR.LOW": "низкий",
+    "ASR.MIDDLE": "средний",
+    "ASR.HIGH": "высокий"
+  },
+  "cs": {
+    "ASR.LOW": "nízký",
+    "ASR.MIDDLE": "střední",
+    "ASR.HIGH": "vysoký"
+  },
+  "it": {
+    "ASR.LOW": "basso",
+    "ASR.MIDDLE": "medio",
+    "ASR.HIGH": "alto"
+  },
+  "pl": {
+    "ASR.LOW": "niski",
+    "ASR.MIDDLE": "średni",
+    "ASR.HIGH": "wysoki"
+  },
+  "tr": {
+    "ASR.LOW": "düşük",
+    "ASR.MIDDLE": "orta",
+    "ASR.HIGH": "yüksek"
+  },
+  "pt": {
+    "ASR.LOW": "baixo",
+    "ASR.MIDDLE": "médio",
+    "ASR.HIGH": "alto"
+  },
+  "hr": {
+    "ASR.LOW": "nisko",
+    "ASR.MIDDLE": "srednje",
+    "ASR.HIGH": "visoko"
+  },
+  "hi": {
+    "ASR.LOW": "कम",
+    "ASR.MIDDLE": "मध्यम",
+    "ASR.HIGH": "उच्च"
+  },
+  "ko": {
+    "ASR.LOW": "낮음",
+    "ASR.MIDDLE": "중간",
+    "ASR.HIGH": "높음"
+  },
+  "th": {
+    "ASR.LOW": "ต่ำ",
+    "ASR.MIDDLE": "ปานกลาง",
+    "ASR.HIGH": "สูง"
+  },
+  "nl": {
+    "ASR.LOW": "laag",
+    "ASR.MIDDLE": "gemiddeld",
+    "ASR.HIGH": "hoog"
+  },
+  "ar": {
+    "ASR.LOW": "منخفض",
+    "ASR.MIDDLE": "متوسط",
+    "ASR.HIGH": "مرتفع"
+  }
 }
 
 const interfaceData = {
-    
+
 }
 
 const alterAndAdd = (jsonData, path) => {
-    let blockPath = path
-    for (let [key, value] of Object.entries(jsonData)) {
-        let path = blockPath + key + '.json'
-        if (Object.keys(value).length) {
-            fs.readFile(path, function (err, data) {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-                let json = JSON.parse(data.toString());
-
-                for (let [index, item] of Object.entries(value)) {
-                    json[index] = item
-                }
-                json = JSON.stringify(json, null, '\t')
-                fs.writeFile(path, json, function (err) {
-                    if (err) {
-                        console.error(err);
-                    }
-                    console.log('--------------------修改成功');
-                    console.log(jsonData);
-                })
-            })
+  let blockPath = path
+  for (let [key, value] of Object.entries(jsonData)) {
+    let path = blockPath + key + '.json'
+    if (Object.keys(value).length) {
+      fs.readFile(path, function (err, data) {
+        if (err) {
+          console.error(err)
+          return
         }
+        let json = JSON.parse(data.toString());
+
+        for (let [index, item] of Object.entries(value)) {
+          json[index] = item
+        }
+        json = JSON.stringify(json, null, '\t')
+        fs.writeFile(path, json, function (err) {
+          if (err) {
+            console.error(err);
+          }
+          console.log('--------------------修改成功');
+          console.log(jsonData);
+        })
+      })
     }
+  }
 }
 
 alterAndAdd(block, './editor/blocks/')
--- src/supported-locales.js
+++ src/supported-locales.js
@@ -38,7 +38,7 @@ const locales = {
     // 'ha': {name: 'Hausa'},
     // 'hy': {name: 'Հայերեն'},
     // 'he': {name: 'עִבְרִית'},
-    // 'hi': {name: 'हिंदी'},
+    'hi': {name: 'हिंदी'},
     'hr': {name: 'Hrvatski'},
     // 'xh': {name: 'isiXhosa'},
     // 'zu': {name: 'isiZulu'},
